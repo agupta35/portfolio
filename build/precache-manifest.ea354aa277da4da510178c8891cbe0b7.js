b69732815006e0f92bea2180004e7f37c189a8c6
agupta35 <agupta35@buffalo.edu>
2020-02-03 15:20:02 -0500

    npm install redo

---
+++ build/precache-manifest.ea354aa277da4da510178c8891cbe0b7.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50287a490ae05db7e7be4773b1bf3115",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "21698c711cf2f9e0f49d",
+    "url": "/portfolio/static/css/2.986bd30c.chunk.css"
+  },
+  {
+    "revision": "a4008de440ca2c4dd433",
+    "url": "/portfolio/static/css/main.bc8f5f15.chunk.css"
+  },
+  {
+    "revision": "21698c711cf2f9e0f49d",
+    "url": "/portfolio/static/js/2.5a38e704.chunk.js"
+  },
+  {
+    "revision": "5adaff15f30f8fb7316ca97fba8ef8b8",
+    "url": "/portfolio/static/js/2.5a38e704.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a4008de440ca2c4dd433",
+    "url": "/portfolio/static/js/main.c9c77655.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "e6956db22f6b55500046d41bb128484a",
+    "url": "/portfolio/static/media/email.e6956db2.svg"
+  },
+  {
+    "revision": "d99368ca4e133a9917838e76243e8cfb",
+    "url": "/portfolio/static/media/facebook.d99368ca.svg"
+  },
+  {
+    "revision": "192a6620a687a9e7e51c0037973a1547",
+    "url": "/portfolio/static/media/github.192a6620.svg"
+  },
+  {
+    "revision": "72d29523d7f200fefdfc952fe57d155e",
+    "url": "/portfolio/static/media/heart.72d29523.svg"
+  },
+  {
+    "revision": "5b23a837268b09d0efa9e307023c1dae",
+    "url": "/portfolio/static/media/instagram.5b23a837.svg"
+  },
+  {
+    "revision": "9e36c8fd15d6f93ed6a45f9484746e8f",
+    "url": "/portfolio/static/media/linkedin.9e36c8fd.svg"
+  },
+  {
+    "revision": "64f273b8b073fb9ed45f41ca7a0a64f2",
+    "url": "/portfolio/static/media/myPicture.64f273b8.jpg"
+  },
+  {
+    "revision": "db17b02c155eed1e10a8474658157d02",
+    "url": "/portfolio/static/media/snow_edited.db17b02c.JPG"
+  },
+  {
+    "revision": "1db9e9c352df34dd25e86db386f3d62b",
+    "url": "/portfolio/static/media/twitter.1db9e9c3.svg"
+  },
+  {
+    "revision": "2e23364a06fd49e0c0f9df52ea819ebd",
+    "url": "/portfolio/static/media/view-projects.2e23364a.jpg"
+  }
+]);
